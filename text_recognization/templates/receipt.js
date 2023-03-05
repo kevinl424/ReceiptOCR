71276bf5bcc9ef7cf6b444d175254725f569c725
kevinl424 <kevin.li.20742@gmail.com>
2023-03-05 00:20:59 -0600

    Updates to js and updating user name input logic

---
+++ text_recognization/templates/receipt.js
@@ -0,0 +1,35 @@
+var url = "";
+var loadFile = function(event) { //initialized beforehand, hoisting
+    var image = document.getElementById('output');
+    url = URL.createObjectURL(event.target.files[0]);
+    image.src = url;
+};
+
+var getNames = function() {
+    var num = document.getElementById('number');
+    num = num.value
+    const location = document.getElementById('people')
+    let htmlString = "";
+    
+    location.innerHTML = ""
+
+    for (let i = 0; i < num; i++) {
+        htmlString += `<div class="names"> <label for="p${i + 1}name">Person ${i + 1} name:</label> <input type="text" id="p${i + 1}name" name="p${i + 1}name"> </div>`
+    }
+    htmlString += '<input type="submit" >'
+    location.innerHTML = htmlString;
+
+}
+
+var lockNames = function(event) {
+    event.preventDefault();
+    const names = document.querySelectorAll(".names")
+
+    for (let i = 0; i < names.length; i++) {
+        var location = document.getElementById(`p${i + 1}name`);
+        console.log(location.value);
+    }
+
+    // const test = document.getElementById('test');
+    // test.innerHTML = document.getElementById('p1name').value
+}
